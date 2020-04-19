3abc6797bbd461efb8fa345b34fc5abe1927e1eb
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-19 20:07:14 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,3 +1,11 @@
 class Hero
   attr_accessor :name, :power, :biography
+
+  HEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @biography = args[:biography]
+    HEROES << self
 end
